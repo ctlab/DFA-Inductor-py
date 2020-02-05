6c45e26d6cef5fda2df66a426354f820af4427e1
Ilya Zakirzyanov <ilya.zakirzyanov@gmail.com>
2020-02-05 23:35:51 +0300

    improved statistics counting performance

--- dfainductor/statistics.py
+++ dfainductor/statistics.py
@@ -1,84 +1,150 @@
 from timeit import default_timer as timer
-
-from typing import Dict, List
+from typing import Dict
 
 from .logging import log_time, log_statistics, log_br
 
 
+# noinspection DuplicatedCode
 class STATISTICS:
-    APTA = 'APTA building'
-    FORMULA = 'Formula building'
-    FEEDING = 'Solver feeding'
-    SOLVING = 'SAT solving'
-    WHOLE = 'Whole task'
+    APTA_NAME = 'APTA building'
+    APTA_SUM = 0
+    APTA_CUR = 0
 
-    times: Dict[str, List[float]] = {}
-    timers: Dict[str, float] = {}
+    FORMULA_NAME = 'Formula building'
+    FORMULA_SUM = 0
+    FORMULA_CUR = 0
+
+    FEEDING_NAME = 'Solver feeding'
+    FEEDING_SUM = 0
+    FEEDING_CUR = 0
+
+    SOLVING_NAME = 'SAT solving'
+    SOLVING_SUM = 0
+    SOLVING_CUR = 0
+
+    WHOLE_NAME = 'Whole task'
+    WHOLE_SUM = 0
+    WHOLE_CUR = 0
+
+    times_sum: Dict[str, float] = {}
+    times_current: Dict[str, float] = {}
 
     @classmethod
     def start_timer(cls, name: str) -> None:
-        if name not in cls.times.keys():
-            cls.times[name] = []
-            cls.timers[name] = 0
-        if cls.timers[name] != 0:
+        if name not in cls.times_sum.keys():
+            cls.times_sum[name] = 0
+            cls.times_current[name] = 0
+        if cls.times_current[name] != 0:
             raise TimerWasNotStoppedBeforeNewStart(name)
-        cls.timers[name] = timer()
+        cls.times_current[name] = timer()
 
     @classmethod
     def stop_timer(cls, name: str) -> float:
-        if name not in cls.times.keys() or cls.timers[name] == 0:
+        end_time = timer()
+        total_time = end_time - cls.times_current[name]
+        if name not in cls.times_sum.keys() or cls.times_current[name] == 0:
             raise TimerWasNotStartedBeforeStopped(name)
-        cls.times[name].append(timer() - cls.timers[name])
-        cls.timers[name] = 0
-        log_time(name + 'time', cls.times[name][-1])
-        return cls.times[name][-1]
+        log_time(name + 'time', total_time)
+        cls.times_sum[name] += total_time
+        cls.times_current[name] = 0
+        return total_time
 
     @classmethod
     def print_statistics(cls):
         log_br()
-        for name, list_of_times in cls.times.items():
-            log_statistics(name, sum(list_of_times))
+        log_statistics(cls.APTA_NAME, cls.APTA_SUM)
+        log_statistics(cls.FORMULA_NAME, cls.FORMULA_SUM)
+        log_statistics(cls.FEEDING_NAME, cls.FEEDING_SUM)
+        log_statistics(cls.SOLVING_NAME, cls.SOLVING_SUM)
+        log_statistics(cls.WHOLE_NAME, cls.WHOLE_SUM)
+        for name, list_of_times in cls.times_sum.items():
+            log_statistics(name, list_of_times)
         log_br()
 
     @classmethod
     def start_apta_building_timer(cls) -> None:
-        cls.start_timer(cls.APTA)
+        if cls.APTA_CUR != 0:
+            raise TimerWasNotStoppedBeforeNewStart(cls.APTA_NAME)
+        cls.APTA_CUR = timer()
 
     @classmethod
     def stop_apta_building_timer(cls) -> float:
-        return cls.stop_timer(cls.APTA)
+        end_time = timer()
+        total_time = end_time - cls.APTA_CUR
+        if cls.APTA_CUR == 0:
+            raise TimerWasNotStartedBeforeStopped(cls.APTA_NAME)
+        log_time(cls.APTA_NAME + 'time', total_time)
+        cls.APTA_SUM += total_time
+        cls.APTA_CUR = 0
+        return total_time
 
     @classmethod
     def start_formula_timer(cls) -> None:
-        cls.start_timer(cls.FORMULA)
+        if cls.FORMULA_CUR != 0:
+            raise TimerWasNotStoppedBeforeNewStart(cls.FORMULA_NAME)
+        cls.FORMULA_CUR = timer()
 
     @classmethod
     def stop_formula_timer(cls) -> float:
-        return cls.stop_timer(cls.FORMULA)
+        end_time = timer()
+        total_time = end_time - cls.FORMULA_CUR
+        if cls.FORMULA_CUR == 0:
+            raise TimerWasNotStartedBeforeStopped(cls.FORMULA_NAME)
+        log_time(cls.FORMULA_NAME + 'time', total_time)
+        cls.FORMULA_SUM += total_time
+        cls.FORMULA_CUR = 0
+        return total_time
 
     @classmethod
     def start_feeding_timer(cls) -> None:
-        cls.start_timer(cls.FEEDING)
+        if cls.FEEDING_CUR != 0:
+            raise TimerWasNotStoppedBeforeNewStart(cls.FEEDING_NAME)
+        cls.FEEDING_CUR = timer()
 
     @classmethod
     def stop_feeding_timer(cls) -> float:
-        return cls.stop_timer(cls.FEEDING)
+        end_time = timer()
+        total_time = end_time - cls.FEEDING_CUR
+        if cls.FEEDING_CUR == 0:
+            raise TimerWasNotStartedBeforeStopped(cls.FEEDING_NAME)
+        log_time(cls.FEEDING_NAME + 'time', total_time)
+        cls.FEEDING_SUM += total_time
+        cls.FEEDING_CUR = 0
+        return total_time
 
     @classmethod
     def start_solving_timer(cls) -> None:
-        cls.start_timer(cls.SOLVING)
+        if cls.SOLVING_CUR != 0:
+            raise TimerWasNotStoppedBeforeNewStart(cls.SOLVING_NAME)
+        cls.SOLVING_CUR = timer()
 
     @classmethod
     def stop_solving_timer(cls) -> float:
-        return cls.stop_timer(cls.SOLVING)
+        end_time = timer()
+        total_time = end_time - cls.SOLVING_CUR
+        if cls.SOLVING_CUR == 0:
+            raise TimerWasNotStartedBeforeStopped(cls.SOLVING_NAME)
+        log_time(cls.SOLVING_NAME + 'time', total_time)
+        cls.SOLVING_SUM += total_time
+        cls.SOLVING_CUR = 0
+        return total_time
 
     @classmethod
     def start_whole_timer(cls) -> None:
-        cls.start_timer(cls.WHOLE)
+        if cls.WHOLE_CUR != 0:
+            raise TimerWasNotStoppedBeforeNewStart(cls.WHOLE_NAME)
+        cls.WHOLE_CUR = timer()
 
     @classmethod
     def stop_whole_timer(cls) -> float:
-        return cls.stop_timer(cls.WHOLE)
+        end_time = timer()
+        total_time = end_time - cls.WHOLE_CUR
+        if cls.WHOLE_CUR == 0:
+            raise TimerWasNotStartedBeforeStopped(cls.WHOLE_NAME)
+        log_time(cls.WHOLE_NAME + 'time', total_time)
+        cls.WHOLE_SUM += total_time
+        cls.WHOLE_CUR = 0
+        return total_time
 
 
 class TimerWasNotStoppedBeforeNewStart(Exception):
