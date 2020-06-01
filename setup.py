7d534bcecc146627dc948782058b935e30f23978
Ilya Zakirzyanov <ilya.zakirzyanov@gmail.com>
2020-06-01 15:42:24 +0300

    first release

--- setup.py
+++ setup.py
@@ -17,12 +17,15 @@ with open(os.path.join(here, 'dfainductor', '__about__.py'), 'r', encoding='utf-
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
+
 setup(
     name=about['__title__'],
     version=about['__version__'],
-    description=about['__description__'],
     author=about['__author__'],
     author_email=about['__author_email__'],
+    description=about['__description__'],
+    long_description=readme,
+    url="https://github.com/ctlab/DFA-Inductor-py",
     license=about['__license__'],
     packages=find_packages(),
     install_requires=requires,
@@ -31,5 +34,15 @@ setup(
         'console_scripts': [
             'dfainductor = dfainductor:cli',
         ]
-    }
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Natural Language :: Russian',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ]
 )
