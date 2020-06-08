9dfed94a84f28ea74c1f84157546e93ddf8dfefc
Ilya Zakirzyanov <ilya.zakirzyanov@gmail.com>
2020-06-08 16:35:08 +0300

    fixed try-catch in main

--- dfainductor/main.py
+++ dfainductor/main.py
@@ -44,67 +44,61 @@ from .structures import APTA, InconsistencyGraph
 def cli(input_: str,
         lower_bound: int,
         upper_bound: int,
-        output: str,
+        output: Optional[str],
         sym_breaking: str,
         solver: str,
         cegar_mode: str,
-        initial_amount: int,
-        step_amount: int,
+        initial_amount: Optional[int],
+        step_amount: Optional[int],
         assumptions_mode: str,
         print_statistics: bool,
         use_ig: bool) -> None:
+    STATISTICS.start_whole_timer()
+    examples_provider = examples.get_examples_provider(input_, cegar_mode, initial_amount, step_amount)
     try:
-        STATISTICS.start_whole_timer()
-        examples_provider = examples.get_examples_provider(input_, cegar_mode, initial_amount, step_amount)
-
         STATISTICS.start_apta_building_timer()
         apta = APTA(examples_provider.get_init_examples())
         log_success('Successfully built an APTA from file \'{0}\''.format(input_))
         log_info('The APTA size: {0}'.format(apta.size))
         STATISTICS.stop_apta_building_timer()
-
-        if use_ig:
-            STATISTICS.start_ig_building_timer()
-        ig = InconsistencyGraph(apta, is_empty=not use_ig)
-        if use_ig:
-            log_success('Successfully built an IG')
-            STATISTICS.stop_ig_building_timer()
-
-        searcher = LSUS(apta,
-                        ig,
-                        solver,
-                        sym_breaking,
-                        cegar_mode,
-                        examples_provider,
-                        assumptions_mode)
-        dfa = searcher.search(lower_bound, upper_bound)
-        if not dfa:
-            log_info('There is no such DFA.')
-        else:
-            if not output:
-                log_br()
-                log_info(str(dfa))
-            else:
-                log_info('Dumping found DFA to {0}'.format(output))
-                try:
-                    with click.open_file(output, mode='w') as file:
-                        file.write(str(dfa))
-                except IOError as err:
-                    log_error('Something wrong with an output file: \'{0}\': {1}'.format(output, err))
-                    log_info('Dumping found DFA to console instead.')
-                    log_br()
-                    log_info(str(dfa))
-            if dfa.check_consistency(examples_provider.get_all_examples()):
-                log_success('DFA is consistent with the given examples.')
-            else:
-                log_error('DFA is not consistent with the given examples.')
-        STATISTICS.stop_whole_timer()
-        if print_statistics:
-            STATISTICS.print_statistics()
     except IOError as err:
         log_error('Cannot build an APTA from file \'{0}\': {1}'.format(input_, err))
         sys.exit(err.errno)
+    if use_ig:
+        STATISTICS.start_ig_building_timer()
+    ig = InconsistencyGraph(apta, is_empty=not use_ig)
+    if use_ig:
+        log_success('Successfully built an IG')
+        STATISTICS.stop_ig_building_timer()
 
-
-if __name__ == "__main__":
-    cli()
+    searcher = LSUS(apta,
+                    ig,
+                    solver,
+                    sym_breaking,
+                    cegar_mode,
+                    examples_provider,
+                    assumptions_mode)
+    dfa = searcher.search(lower_bound, upper_bound)
+    if not dfa:
+        log_info('There is no such DFA.')
+    else:
+        if not output:
+            log_br()
+            log_info(str(dfa))
+        else:
+            log_info('Dumping found DFA to {0}'.format(output))
+            try:
+                with click.open_file(output, mode='w') as file:
+                    file.write(str(dfa))
+            except IOError as err:
+                log_error('Something wrong with an output file: \'{0}\': {1}'.format(output, err))
+                log_info('Dumping found DFA to console instead.')
+                log_br()
+                log_info(str(dfa))
+        if dfa.check_consistency(examples_provider.get_all_examples()):
+            log_success('DFA is consistent with the given examples.')
+        else:
+            log_error('DFA is not consistent with the given examples.')
+    STATISTICS.stop_whole_timer()
+    if print_statistics:
+        STATISTICS.print_statistics()
